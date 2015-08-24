3469ca0bb38f3346ad40d9e3eb638bbce09fd06b
Ohtsuka Shingo <capriciouswalk@gmail.com>
2015-08-24 15:20:26 +0900

    SimpleProblem の実装

---
+++ src/Problem/SimpleProblem.cpp
@@ -0,0 +1,31 @@
+#include "SimpleProblem.h"
+#include <iostream>
+
+template <class F, class S>
+SimpleProblem<F, S>::SimpleProblem() : field(new F()), stones([](){
+  std::vector<std::shared_ptr<Stone>> list;
+  int num;
+  std::cin >> num;
+  for(int i = 0; i < num; ++i) {
+    std::shared_ptr<Stone> stone(new S());
+  }
+  return list;
+}()) {}
+
+
+template <class F, class S>
+std::unique_ptr<Field> SimpleProblem<F, S>::get_field()
+{
+  return std::move(field -> clone());
+}
+
+template <class F, class S>
+std::vector<std::weak_ptr<Stone>> SimpleProblem<F, S>::get_stones()
+{
+  std::vector<std::weak_ptr<Stone>> list;
+  for(auto stone : stones) {
+    list.push_back(stone);
+  }
+  return list;
+}
+
---
+++ src/Problem/SimpleProblem.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Problem.h"
+
+template <class F, class S>
+class SimpleProblem : public Problem<F, S> {
+  private:
+    std::unique_ptr<Field> field;
+    std::vector<std::shared_ptr<Stone>> stones;
+
+  public:
+    SimpleProblem();
+    virtual ~SimpleProblem() {};
+    virtual std::unique_ptr<Field> get_field();
+    virtual std::vector<std::weak_ptr<Stone>> get_stones();
+};
+
