da0300bc114138c6a118253f02b603a5535b2d3b
Ohtsuka Shingo <capriciouswalk@gmail.com>
2015-08-24 17:08:16 +0900

    Problem の整え始め

--- Problem/include/problem.h
+++ Problem/include/problem.h
@@ -3,11 +3,13 @@
 #include <vector>
 #include <memory>
 #include <type_traits>
-#include "../Field/field.h"
-#include "../Stone/stone.h"
+#include "field.h"
+#include "stone.h"
 
 template <class F, class S>
 class Problem {
+  static_assert(std::is_base_of<Field, F>::value, "class F maybe not base of Field");
+  static_assert(std::is_base_of<Stone, S>::value, "class S maybe not base of Stone");
   public:
     virtual ~Problem() {};
     virtual std::unique_ptr<Field> get_field() = 0;
