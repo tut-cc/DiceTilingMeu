027c1bfef1b37fc525e077b02721d7b4793ea298
foldori <origaminomiko@gmail.com>
2015-08-31 10:12:32 +0900

    バグを修正

--- Algorithm/include/detail/Beamalgo.hpp
+++ Algorithm/include/detail/Beamalgo.hpp
@@ -10,10 +10,16 @@ Beamalgo<F, S>::Beamalgo(std::shared_ptr<Problem> p)
 		stones.push_back(std::shared_ptr<Stone>(new S(s)));
 	}
 }
+template <class F, class S>
+static int Beamalgo<F, S>::h(std::shared_ptr<Stone> stone){
+	return 0;
+}
+
 
 template <class F, class S>
 void Beamalgo<F, S>::solve()
 {
+	/*
 	for (int i = 0; i < 32; i++){
 		for (int j = 0; j < 32; j++){
 			
@@ -21,5 +27,17 @@ void Beamalgo<F, S>::solve()
 		}
 		std::cout << std::endl;
 	}
+
+	for (int angle = 0; angle < 4; angle++){
+		for (int i = 0; i < 8; i++){
+			for (int j = 0; j < 8; j++){
+				std::cout << stones[0]->at(i, j, angle);
+			}
+			std::cout << std::endl;
+		}
+		std::cout << std::endl;
+	}
+	*/
+
 }
 
