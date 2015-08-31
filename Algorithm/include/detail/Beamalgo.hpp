3b7467414a6377f2ed972fcd98033944739b1d40
foldori <origaminomiko@gmail.com>
2015-08-31 20:39:01 +0900

    コンパイル通るようになった

--- Algorithm/include/detail/Beamalgo.hpp
+++ Algorithm/include/detail/Beamalgo.hpp
@@ -8,7 +8,7 @@ Beamalgo<F, S>::Beamalgo(std::shared_ptr<Problem> p)
 {
 	field = std::move(std::unique_ptr<Field>(new F(p->get_field_str())));
 	for (auto&& s : p->get_stones_str()) {
-		stones.push_back(std::shared_ptr<Stone>(new S(s)));
+		stones.push_back(std::shared_ptr<Stone>(new S(s, (int)(stones.size()))));
 	}
 }
 
@@ -38,36 +38,48 @@ void Beamalgo<F, S>::solve()
 	
 	std::priority_queue<std::unique_ptr<Field>> s_list;
 	s_list.push(field->clone());
-
+	
+	
 	for (auto& st : stones){
 		std::priority_queue<std::unique_ptr<Field>> tmp_list;
-
+		
 		for (int si = 0; si < (int)(s_list.size());si++){
-			std::unique_ptr<Field> s = s_list.top();
+			std::unique_ptr<Field> s = std::move(s_list.top());
 			s_list.pop();
-			for (int i = -7; i <= 7; i++){
-				for (int j = -7; j <= 7; j++){
-					for (int a = 0; a < 4; a++){
+			
+			for (int i = 0; i < 32; i++){
+				for (int j = -7; j < 32; j++){
+					for (int a = -7; a < 4; a++){
 						for (int r = 0; r < 2; r++){
+							
 							if (s->appliable(st, i, j, r, a)){
-								std::unique_ptr<Field> tmp = s->clone();
+								
+								std::unique_ptr<Field> tmp = field->clone();
+								
 								tmp->apply(st, i, j, r, a);
+								
 								if (tmp_list.size() >= BEAM_WIDTH){
 									tmp_list.pop();
 								}
-								tmp_list.push(tmp);
+								
+								tmp_list.push(std::move(tmp));
+								
 							}
+							
 						}
 					}
 				}
 			}
+			
 		}
 		
-		s_list = tmp_list;
+		s_list = std::move(tmp_list);
 	}
+	
+	
 	int size = (int)(s_list.size());
 	for (int i = size - 1; i >= 0; i--){
-		std::unique_ptr<Field> tmp = s_list.top();
+		std::unique_ptr<Field> tmp = std::move(s_list.top());
 		for (int i = 0; i < 32; i++){
 			for (int j = 0; j < 32; j++){
 
