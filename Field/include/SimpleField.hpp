e248b504ce64df1fd34f78b6666734e93c1fd48f
Ohtsuka Shingo <capriciouswalk@gmail.com>
2015-08-31 15:43:55 +0900

    add final/override keyword

--- Field/include/SimpleField.hpp
+++ Field/include/SimpleField.hpp
@@ -11,9 +11,9 @@ class SimpleField : public Field {
     SimpleField(std::vector<std::string>);
     SimpleField(const bool mat[32][32]);
     virtual ~SimpleField() = default;
-    virtual bool at(int x, int y) const;
-    virtual bool appliable(std::weak_ptr<Stone> s, int x, int y, int reverse, int angle) const;
-    virtual void apply(std::weak_ptr<Stone> s, int x, int y, int reverse, int angle);
-    virtual std::unique_ptr<Field> clone() const;
+    virtual bool at(int x, int y) const override;
+    virtual bool appliable(std::weak_ptr<Stone> s, int x, int y, int reverse, int angle) const override;
+    virtual void apply(std::weak_ptr<Stone> s, int x, int y, int reverse, int angle) override;
+    virtual std::unique_ptr<Field> clone() const override;
 };
 
