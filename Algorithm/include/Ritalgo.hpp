8785867d065742603531b9839cfa23e1f9b3d385
Ohtsuka Shingo <capriciouswalk@gmail.com>
2015-09-01 10:47:01 +0900

    ACOの大まかな枠決めとTODOの設定

--- Algorithm/include/Ritalgo.hpp
+++ Algorithm/include/Ritalgo.hpp
@@ -6,13 +6,70 @@
 #include "problem.hpp"
 #include <memory>
 #include <vector>
+#include <type_traits>
+#include <iostream>
 
 template <class F, class S>
 class Ritalgo : public Algorithm {
-  std::unique_ptr<Field> field;
-  std::vector<std::shared_ptr<Stone>> stones;
+  private:
+    std::unique_ptr<Field> field;
+    std::vector<std::shared_ptr<Stone>> stones;
+    class Env {
+      private:
+        static constexpr double EVAPORATE_RATE = 0.98;
+        union {
+          double env[256][2][2][64][64][2][4];
+          double raw[256 *2 *2 *64 *64 *2 *4];
+        };
+/* VER : heap
+        double *******env;
+
+        template <typename T, typename TSize>
+        T make(T& ptr, const TSize ts);
+        template <typename T, typename TSize, typename... RSize>
+        T make(T& ptr, const TSize ts, const RSize... rs);
+
+        template <typename T, typename TSize>
+        T make(T& ptr, const std::shared_ptr<double>, const TSize ts);
+        template <typename T, typename TSize, typename... RSize>
+        T make(T& ptr, const std::shared_ptr<double>, const TSize ts, const RSize... rs);
+
+        template <typename T, typename TSize>
+        T destroy(T& ptr, const TSize ts);
+        template <typename T, typename TSize, typename... RSize>
+        T destroy(T& ptr, const TSize ts, const RSize... rs);
+*/
+
+        template <typename T, typename TSize>
+        static void eva(T& ptr, const TSize ts);
+        template <typename T, typename TSize, typename... RSize>
+        static void eva(T& ptr, const TSize ts, const RSize... rs);
+      public:
+        Env() = default;
+        ~Env() = default;
+/* VER : heap
+        Env();
+        ~Env();
+*/
+        void put(int idx, int is, int fir, int x, int y, int rev, int ang, double phe);
+        double get(int idx, int is, int fir, int x, int y, int rev, int ang) const;
+        void eva();
+    };
+    class Ant {
+      private:
+        static constexpr double PHEROMONE_VOLUME = 1.0;
+        static constexpr double ALPHA = 1.0;
+        static constexpr double BETA = 0.5;
+        std::unique_ptr<Field> field;
+        std::vector<std::shared_ptr<Stone>> stones;
+      public:
+        Ant(const std::unique_ptr<Field> field, const std::vector<std::shared_ptr<Stone>> & stones);
+        ~Ant() = default;
+        void run();
+        void renew(std::shared_ptr<Env> env);
+    };
   public:
-    Ritalgo(std::shared_ptr<Problem> p);
+    Ritalgo(const std::shared_ptr<Problem> p);
     virtual ~Ritalgo() = default;
     virtual void solve();
 };
--- Algorithm/include/detail/Ritalgo.hpp
+++ Algorithm/include/detail/Ritalgo.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "Ritalgo.hpp"
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 
 template <class F, class S>
 Ritalgo<F, S>::Ritalgo(std::shared_ptr<Problem> p)
@@ -14,6 +16,138 @@ Ritalgo<F, S>::Ritalgo(std::shared_ptr<Problem> p)
 template <class F, class S>
 void Ritalgo<F, S>::solve()
 {
-  std::cout << "チェストー" << std::endl;
+  std::shared_ptr<Env> env(new Env());
+  std::vector<std::shared_ptr<Ant>> ants;
+  for (int i = 0; i < 100; ++i) {
+    std::shared_ptr<Ant> ptr(new Ant(field -> clone(), stones));
+    ants.push_back(ptr);
+  }
+  std::cerr << "チェストー" << std::endl;
+  for (;;) {
+    for (auto ant : ants) {
+      ant -> run();
+    }
+    for (auto ant : ants) {
+      ant -> renew(env);
+    }
+    env -> eva();
+  }
+}
+
+/*
+template <class F, class S>
+template <typename T, typename TSize>
+T Ritalgo<F, S>::Env::make(T& ptr, const TSize ts) {
+  ptr = new typename std::remove_pointer<T>::type [ts];
+  for (int i = 0; i < ts; ++i) {
+    ptr[i] = 0.0;
+  }
+  return ptr;
+}
+
+template <class F, class S>
+template <typename T, typename TSize, typename... RSize>
+T Ritalgo<F, S>::Env::make(T& ptr, const TSize ts, const RSize... rs) {
+  ptr = new typename std::remove_pointer<T>::type [ts];
+  for (int i = 0; i < ts; ++i) {
+    ptr[i] = make(ptr[i], rs...);
+  }
+  return ptr;
+}
+
+template <class F, class S>
+template <typename T, typename TSize>
+T Ritalgo<F, S>::Env::destroy(T& ptr, const TSize ts) {
+  for (int i = 0; i < ts; ++i) {
+    ptr[i] = 0;
+  }
+  delete ptr;
+  ptr = nullptr;
+  return ptr;
+}
+
+template <class F, class S>
+template <typename T, typename TSize, typename... RSize>
+T Ritalgo<F, S>::Env::destroy(T& ptr, const TSize ts, const RSize... rs) {
+  for (int i = 0; i < ts; ++i) {
+    ptr[i] = destroy(ptr[i], rs...);
+  }
+  delete ptr;
+  ptr = nullptr;
+  return ptr;
+}
+
+
+template <class F, class S>
+Ritalgo<F, S>::Env::Env()
+{
+  env = make(env, 256, 2, 2, 64, 64, 2, 4);
+}
+
+template <class F, class S>
+Ritalgo<F, S>::Env::~Env()
+{
+  env = destroy(env, 256, 2, 2, 64, 64, 2, 4);
+}
+*/
+
+template <class F, class S>
+void Ritalgo<F, S>::Env::put(int idx, int is, int fir, int x, int y, int rev, int ang, double phe)
+{
+  env[idx][is][fir][x][y][rev][ang] += phe;
+}
+
+template <class F, class S>
+double Ritalgo<F, S>::Env::get(int idx, int is, int fir, int x, int y, int rev, int ang) const
+{
+  return env[idx][is][fir][x][y][rev][ang];
+}
+
+template <class F, class S>
+template <typename T, typename TSize>
+void Ritalgo<F, S>::Env::eva(T& ptr, const TSize ts)
+{
+  for (int i = 0; i < ts; ++i) {
+    ptr[i] *= EVAPORATE_RATE;
+  }
+}
+
+template <class F, class S>
+template <typename T, typename TSize, typename... RSize>
+void Ritalgo<F, S>::Env::eva(T& ptr, const TSize ts, const RSize... rs)
+{
+  for (int i = 0; i < ts; ++i) {
+    eva(ptr[i], rs...);
+  }
+}
+
+template <class F, class S>
+void Ritalgo<F, S>::Env::eva()
+{
+  eva(env, 256, 2, 2, 64, 64, 2, 4);
+}
+
+
+template <class F, class S>
+Ritalgo<F, S>::Ant::Ant(std::unique_ptr<Field> field, const std::vector<std::shared_ptr<Stone>> & stones)
+{
+  this -> field = std::move(field);
+  std::copy(stones.begin(), stones.end(), std::back_inserter(this -> stones));
+}
+
+template <class F, class S>
+void Ritalgo<F, S>::Ant::run()
+{
+  // TODO:
+}
+
+template <class F, class S>
+void Ritalgo<F, S>::Ant::renew(std::shared_ptr<Env> env)
+{
+  int index = 0;
+  for ( auto e : field -> get_history() ) {
+    // TODO:
+    // WANTED: Field -> score
+  }
 }
 
