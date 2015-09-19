5d95bb51d2e302e73ccc4a15e4c16e2c2a58e446
Shingo Ohtsuka <capriciouswalk@gmail.com>
2015-09-19 14:49:36 +0900

    feature/Ritalgo分

--- Stone/include/SimpleStone.hpp
+++ Stone/include/SimpleStone.hpp
@@ -6,9 +6,11 @@
 class SimpleStone : public Stone {
   private:
     bool mat[4][2][8][8];
+    std::pair<double, double> centers[4][2];
   public:
     SimpleStone(std::vector<std::string>, int index);
     virtual ~SimpleStone() = default;
     virtual bool at(int x, int y, int reverse, int angle) const override;
+    virtual std::pair<double, double> center(int reverse, int angle) const override;
 };
 
